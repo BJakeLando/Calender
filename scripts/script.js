c07a16e54f2ffb93b690b7c957104c9421774c15
BJakeLando <bjlando@gmail.com>
2022-09-24 11:33:20 -0700

    Assignment 3
    Display tasks on HTML using jquery

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,48 @@ var isVisible = true;
 
 
 function saveTask() {
-    console.log("Button clicked");
+    
+    let title = $("#txtTitle").val();
+    let description = $("#txtDescription").val();
+    let dueDate = $("#txtDueDate").val();
+    let tag = $("#txtTag").val();
+    let category = $("#txtCategory").val();
+    let color = $("#txtColor").val();
+
+    // console.log("values", title, description,dueDate,tag,category,color);
+
+    let task= new Task(title,description,dueDate,tag,color,category);
+
+    displayTask(task);
+    clearForm(task);
+
+}
+
+function displayTask(task) {
+    console.log(task);
+
+    let syntax =`
+        <div class="task"> 
+            <div id="task-header">
+                <h3>${task.title}</h3> 
+                <p id="taskDate">${task.dueDate}</p> 
+            </div>
+            <div>
+                <h3 id=taskCategory>${task.category}</h3> 
+                <p>${task.tag}</p> 
+            </div>
+        </div>`;
+
+    $("#taskList").append(syntax)
+}
+
+function clearForm() {
+    
+    $("#txtTitle").val("");
+    $("#txtDescription").val("");
+    $("#txtDueDate").val("");
+    $("#txtCategory").val("");
+    
 
 }
 
---
+++ scripts/task.js
@@ -0,0 +1,23 @@
+
+class Task {
+
+    constructor(title,desc,dueDate,tag,color,cat){
+
+        this.title = title;
+        this.description = desc;
+        this.dueDate= dueDate;
+        this.tag= tag;
+        this.color= color;
+        this.category = cat;
+        
+
+        this.name= "Brandon";
+
+    }
+
+}
+
+function init() {
+}
+
+window.onload=init;
