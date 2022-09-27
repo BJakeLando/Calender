5f642ad78b9c91d688a90c53d493d450145cd5b1
BJakeLando <bjlando@gmail.com>
2022-09-26 19:58:33 -0700

    FSDI 106 Competency Report
    Competency Report

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,43 @@
-
-var importantIcon= "fas fa-star";
-var nonImportantIcon= "far fa-star";
+var importantIcon = "fas fa-star";
+var nonImportantIcon = "far fa-star";
 var isImportant = false;
 
 var isVisible = true;
 
-
-
-
 function saveTask() {
-    
-    let title = $("#txtTitle").val();
-    let description = $("#txtDescription").val();
-    let dueDate = $("#txtDueDate").val();
-    let tag = $("#txtTag").val();
-    let category = $("#txtCategory").val();
-    let color = $("#txtColor").val();
+  let title = $("#txtTitle").val();
+  let description = $("#txtDescription").val();
+  let dueDate = $("#txtDueDate").val();
+  let tag = $("#txtTag").val();
+  let category = $("#txtCategory").val();
+  let color = $("#txtColor").val();
 
-    // console.log("values", title, description,dueDate,tag,category,color);
+  // console.log("values", title, description,dueDate,tag,category,color);
 
-    let task= new Task(title,description,dueDate,tag,color,category);
+  let task = new Task(title, description, dueDate, tag, color, category);
 
-    displayTask(task);
-    clearForm(task);
-
+  //save on server
+  $.ajax({
+    type: "POST",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    data: JSON.stringify(task),
+    contentType: "application/json;",
+    success: function (response) {
+      displayTask(task);
+      clearForm(task);
+      console.log("server says", response);
+    },
+    error: function (details) {
+      console.log("save failed", details);
+      alert("Error, we could not save your task");
+    },
+  });
 }
 
 function displayTask(task) {
-    console.log(task);
+  console.log(task);
 
-    let syntax =`
+  let syntax = `
         <div class="task"> 
             <div id="task-header">
                 <h3>${task.title}</h3> 
@@ -41,57 +49,90 @@ function displayTask(task) {
             </div>
         </div>`;
 
-    $("#taskList").append(syntax)
+  $("#taskList").append(syntax);
 }
 
 function clearForm() {
-    
-    $("#txtTitle").val("");
-    $("#txtDescription").val("");
-    $("#txtDueDate").val("");
-    $("#txtCategory").val("");
-    
-
+  $("#txtTitle").val("");
+  $("#txtDescription").val("");
+  $("#txtDueDate").val("");
+  $("#txtCategory").val("");
 }
 
-function iconUI(){
-    if (!isImportant) {
-        //change to important
-        $("#iImportant").removeClass(nonImportantIcon).addClass(importantIcon);
-        isImportant = true;
-    } else {
-        // change to non important 
-        $("#iImportant").removeClass(importantIcon).addClass(nonImportantIcon);
-        isImportant = false;
-    
-    }
+function iconUI() {
+  if (!isImportant) {
+    //change to important
+    $("#iImportant").removeClass(nonImportantIcon).addClass(importantIcon);
+    isImportant = true;
+  } else {
+    // change to non important
+    $("#iImportant").removeClass(importantIcon).addClass(nonImportantIcon);
+    isImportant = false;
+  }
 }
-
 
 function hideTask() {
-    if(isVisible) {
-        //hide it
-        $(".info").hide();
-        isVisible = false;
-    }
-    else {
-        //show it
-        $(".info").show();
-        isVisible = true;
-    }
+  if (isVisible) {
+    //hide it
+    $(".info").hide();
+    isVisible = false;
+  } else {
+    //show it
+    $(".info").show();
+    isVisible = true;
+  }
+}
 
+function fetchTasks() {
+  $.ajax({
+    type: "GET",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    success: function (response) {
+      let allTasks = JSON.parse(response);
+
+      //Travel the array
+      //Get every task and send it to displayTask
+      for (let i = 0; i < allTasks.length; i++) {
+        let task = allTasks[i];
+        //display only your tasks
+        //if the name on the task is equal to my name
+        if (task.name == "Brandon") {
+          displayTask(task);
+        }
+      }
+    },
+    error: function (data) {
+      console.log(data);
+    },
+  });
+}
+
+function testRequest() {
+  // this is a test http request
+  $.ajax({
+    type: "GET",
+    url: "https://fsdiapi.azurewebsites.net/",
+    success: function (data) {
+      //Respons from the server
+      console.log(data);
+    },
+    error: function (details) {
+      //details about error
+      console.log("Error", details);
+    },
+  });
 }
 
 function init() {
-    console.log("Task Manager");
-// load prev data
+  console.log("Task Manager");
 
-// catch events
-$("#btnSave").click(saveTask);
+  // load prev data
+  fetchTasks();
 
-$("#iImportant").click(iconUI);
-$("#hideTask").click(hideTask);
-
+  // catch events
+  $("#btnSave").click(saveTask);
+  $("#iImportant").click(iconUI);
+  $("#hideTask").click(hideTask);
 }
 
-window.onload=init;
+window.onload = init;
--- scripts/task.js
+++ scripts/task.js
@@ -1,23 +1,16 @@
-
 class Task {
+  constructor(title, desc, dueDate, tag, color, cat) {
+    this.title = title;
+    this.description = desc;
+    this.dueDate = dueDate;
+    this.tag = tag;
+    this.color = color;
+    this.category = cat;
 
-    constructor(title,desc,dueDate,tag,color,cat){
-
-        this.title = title;
-        this.description = desc;
-        this.dueDate= dueDate;
-        this.tag= tag;
-        this.color= color;
-        this.category = cat;
-        
-
-        this.name= "Brandon";
-
-    }
-
+    this.name = "Brandon";
+  }
 }
 
-function init() {
-}
+function init() {}
 
-window.onload=init;
+window.onload = init;
